b3437b6f03947f1947e48c5d8d4b8d0eadd2f509
Franklin Chieze <chieze.franklin@gmail.com>
2017-05-08 10:46:14 +0100

    Rebuilt for Heroku

--- config.js
+++ config.js
@@ -3,9 +3,6 @@
 var fs = require("fs");
 var path = require("path");
 
-var __host;
-var __port;
-var __ports;
 var __config;
 var getConfig = function(){
 	if(__config) return __config;
@@ -20,39 +17,11 @@ var getConfig = function(){
 }
 
 module.exports = {
-	getAppPortEnd : function(){ //deprecated (for now)
-		return getConfig().appPortEnd;
-	},
-	getAppPorts : function(){ //deprecated (for now); see {root folder}/__raw/ports2.js for how it was used
-		if(__ports) return __ports;
-
-		__ports = [];
-		for(var i = getConfig().appPortStart; i <= getConfig().appPortEnd; i++){
-			__ports.push(i);
-		}
-		return __ports;
-	},
-	getAppPortStart : function(){ //deprecated (for now)
-		return getConfig().appPortStart;
-	},
-
-	getDbHost : function(){
-		return getConfig().dbHost;
-	},
-	getDbPort : function(){
-		return getConfig().dbPort;
-	},
 	getFriendlyName : function(){
 		return getConfig().friendlyName;
 	},
 	getFriendlyVersion : function(){
 		return getConfig().friendlyVersion;
-	},
-	getHost : function(){
-		if(__host) return __host;
-
-		__host = getConfig().host;
-		return __host;
 	},
 	getMinimumVersion : function(){
 		return getConfig().minimumVersion;
@@ -63,26 +32,7 @@ module.exports = {
 	getName : function(){
 		return getConfig().name;
 	},
-	getPort : function(){
-		if(__port) return __port;
-
-		__port = getConfig().port;
-		return __port;
-	},
-	getProtocol : function(){
-		return getConfig().protocol;
-	},
-	getSessionSecret : function(){
-		return getConfig().sessionSecret;
-	},
 	getVersion : function(){
 		return getConfig().version;
-	},
-
-	setHost : function(host){
-		__host = host;
-	},
-	setPort : function(port){
-		__port = port;
 	}
 };
